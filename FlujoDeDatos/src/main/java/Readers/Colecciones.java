e6e7f07258cb119dad5aed03f6499fa4651597f9
lauticelico <lautaro.celicoet32@gmail.com>
2024-04-24 12:46:28 -0300

    listas
    explicacion de listas

--- FlujoDeDatos/src/main/java/Readers/Colecciones.java
+++ FlujoDeDatos/src/main/java/Readers/Colecciones.java
@@ -12,16 +12,21 @@ import java.util.Set;
 
 public class Colecciones {
 
+	//Colecciones son variables donde se pueden guardar mas de 1 dato a la vez
+	
 	//vector
 	//int[] vector = new int[num];
 	
+	//menos eficiente para agregar
 	ArrayList<String> frutas = new ArrayList<>();
-	//ArrayList verduras = ArrayList<>();
+	ArrayList verduras = new ArrayList<>();
 	
 	List nombre = new ArrayList<>();
 	List<String> apellido = new ArrayList<>();
 	
+	//muy eficientes para agregar información
 	LinkedList<String> componentesPc = new LinkedList<>();
+	List precios = new LinkedList<>();
 	
 	LinkedHashMap<String, String> listaEspañolIngles = new LinkedHashMap<>();
 	Map<Integer,Integer> valores = new LinkedHashMap<>();
@@ -29,11 +34,35 @@ public class Colecciones {
 	LinkedHashSet<Integer> listaEspañol = new LinkedHashSet<>();
 	Set<Integer> valores2 = new LinkedHashSet<>();
 	
-	
+	//        k   ,   v
 	HashMap<String, String> españolRAE = new HashMap<>();
-	
 	Map<String,Integer> españolIngles = new HashMap();
+	
 	
 	HashSet<String> claves = new HashSet<>();
 	Set<Integer> DNI = new HashSet<>();
+	
+	//-----------------------------------------------
+	
+	public void Array()
+	{
+		frutas.add("s");
+		verduras.add(5);
+		verduras.add("s");
+		
+		frutas.clear(); //limpia todo lo de adentro
+		frutas.remove("manzana"); //lo que esta en la posicion 5
+		frutas.remove("manzana"); // busca y borra ese dato particular
+		
+		verduras.contains("tomate"); //existe o no
+		verduras.containsAll( new ArrayList<>());
+		
+		frutas.get(5); //devuelve lo que esta en la posicion 5
+		frutas.indexOf("banana"); //buscar banana y devuelve la posicion
+	
+		verduras.isEmpty();
+		verduras.size();
+		
+	
+	
 }
